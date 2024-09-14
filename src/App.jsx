0e014b503faf170d8ce28a3bf894b87c3f13e7df
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-09-14 12:35:31 +0600

    added App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-// import AboutUmra from './components/about-umra/AboutUmra.jsx';
-// import Header from './components/header/Header.jsx';
-// import Home from './components/home/Home.jsx';
-// import Price from './components/price/Price.jsx';
-// import VideoUmra from './components/video-umra/VideoUmra.jsx';
-// import Contackts from './components/contackts/Contackts.jsx';
-// import Documents from './components/documents/Documents.jsx';
-// import Hadis from './components/hadis/Hadis.jsx';
-// import Questions from './components/questions/Questions.jsx';
+import AboutUmra from './components/about-umra/AboutUmra.jsx';
+import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
+import Price from './components/price/Price.jsx';
+import VideoUmra from './components/video-umra/VideoUmra.jsx';
+import Contackts from './components/contackts/Contackts.jsx';
+import Documents from './components/documents/Documents.jsx';
+import Hadis from './components/hadis/Hadis.jsx';
+import Questions from './components/questions/Questions.jsx';
 import QuiteWithPost from './components/quite-with-post/QuiteWithPost.jsx';
 import Review from './components/review/Review.jsx';
 
 const App = () => {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Home />
       <AboutUmra />
-      <VideoUmra/>
+      <VideoUmra />
       <Price />
-      <Contackts/>
-      <Documents/>
-      <Hadis /> */}
-      {/* <Questions /> */}
-      <Review/>
-      <QuiteWithPost/>
+      <Contackts />
+      <Documents />
+      <Hadis />
+      <Questions />
+      <Review />
+      <QuiteWithPost />
     </>
   );
 };
